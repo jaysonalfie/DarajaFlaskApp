6f6221735d6d4f81f399f153b0bd1fa55e99b963
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-07-08 14:25:03 +0300

    updated hypercorn

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def access_token():
 
 #Ensure app runs only if script is executed directly
 if __name__ == '__main__':
-    app.run(host ="0.0.0.0", port = 8000, debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
